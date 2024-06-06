4318812a88c55b2c9bf6fc7010ead168bac9997d
Sarkhan <mamedov040894@gmail.com>
2024-06-06 23:37:53 +0300

    Practic_Task

---
+++ src/Lesson3.java
@@ -0,0 +1,9 @@
+public class Lesson3 {
+    public static void main (String[] args){
+        System.out.println("Свойства цифра: 454");
+        int a = 15;
+        int b = 5;
+        System.out.println("Количество цифр: " + (a / b ));
+
+    }
+}
