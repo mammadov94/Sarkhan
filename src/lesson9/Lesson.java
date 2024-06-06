ba0daec80248c210a28c809433baaa3c1f4d2c1e
Sarkhan <mamedov040894@gmail.com>
2024-06-06 23:53:10 +0300

    Lesson_9

--- src/lesson9/Lesson.java
+++ src/lesson9/Lesson.java
@@ -1,7 +1,6 @@
 package lesson9;
 
 public class Lesson {
-
     public static void main (String[] args){
         int a = 3;
         for (int i = 1; i <= 10; i++){
--- src/lesson9/Lesson3.java
+++ src/lesson9/Lesson3.java
@@ -1,7 +1,6 @@
 package lesson9;
 
 public class Lesson3 {
-
     public static void main (String[] args){
         System.out.println("Свойства цифра: 454");
         int a = 15;
