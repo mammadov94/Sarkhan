35d668cd7b52c14a65af50177c12f582c6fd6625
Sarkhan <mamedov040894@gmail.com>
2024-06-18 01:12:54 +0300

    Lesson 13 Bank App

---
+++ src/Lesson13/BankBranch.java
@@ -0,0 +1,36 @@
+package Lesson13;
+
+public class BankBranch {
+    private Long id;
+    private String adress;
+    private Bank bank;
+
+    public BankBranch(Long id,String adress) {
+        this.id = id;
+        this.adress = adress;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    @Override
+    public String toString() {
+        return "BankBranch{" +
+                "id=" + id +
+                ", adress='" + adress + '\'' +
+                '}';
+    }
+}
